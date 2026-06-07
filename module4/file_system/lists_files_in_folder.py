0dfaed4e88477a4c1b08f51309e6abfa5dd556d1
Ridoy Dey <110971349+iamridoydey@users.noreply.github.com>
2026-06-07 16:27:15 +0000

    Lists files in folder

---
+++ module4/file_system/lists_files_in_folder.py
@@ -0,0 +1,15 @@
+import os
+
+def list_files_in_folder(path: str): 
+    try:
+        files = os.listdir(path)
+        return files, None
+    except FileNotFoundError:
+        return None, "File not found"
+    except PermissionError:
+        return None, "Permission denied"
+    
+if __name__ == "__main__":
+    files, error = list_files_in_folder("/workspaces/python_automation")
+    print("Files: ", files)
+    print("Error: ", error)
